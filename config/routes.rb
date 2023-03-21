7f991f72eb90178ed587b468ccb73e232f6ffcd9
Ai1023 <126910091+Ai1023@users.noreply.github.com>
2023-03-21 08:35:11 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  resources :books, only: [:new, :create, :index, :show, :edit]
+  get "/homes/about" => "homes#about", as: "about"
+  get "/homes/index" => "homes#index", as: "index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
